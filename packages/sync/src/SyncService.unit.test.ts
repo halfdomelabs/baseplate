b0f1fe4d622bfda3da7ebcd1374b6f4df4e0bfb8
Kingston <web@ktam.org>
2020-02-24 18:59:28 +0100

    Add initial SyncService implementation
    + Adds loadProject and build implementations
    + Swaps out mockfs for memfs as it requires less hackery
    + Add type for Module

--- packages/sync/src/SyncService.unit.test.ts
+++ packages/sync/src/SyncService.unit.test.ts
@@ -1,22 +1,101 @@
-import mock from 'mock-fs';
+import { Volume } from 'memfs';
+import * as yup from 'yup';
+import createPromisesApi from 'memfs/lib/promises';
+import { Module, Descriptor, Action } from './types';
 import { SyncService } from './SyncService';
 
+function generateMockAction(action?: Partial<Action>): Action {
+  return {
+    name: 'test-action',
+    displayName: 'Test Action',
+    description: 'Test Description',
+    // eslint-disable-next-line @typescript-eslint/require-await
+    execute: async () => {},
+    ...action,
+  };
+}
+
 describe('load descriptor', () => {
-  test('should load a plain descriptor', () => {
-    mock({
+  interface CustomDescriptor extends Descriptor {
+    customString: string;
+    customObject: {
+      customNull?: string;
+    };
+  }
+
+  test('should load a descriptor with custom fields', async () => {
+    const vol = Volume.fromJSON({
       '/workspace/baseplate/project.json': JSON.stringify({
-        module: '@baseplate/sync:workspace',
+        module: 'core:test',
         name: 'myProject',
+        customString: 'customValue',
+        customObject: {
+          customNull: null,
+        },
       }),
     });
 
-    const descriptor = SyncService.loadDescriptor('/workspace');
+    const service = new SyncService(
+      {
+        'core:test': {
+          name: 'test',
+          descriptorSchema: {
+            customString: yup.string().required(),
+            customObject: yup.object({
+              customNull: yup.string().nullable(),
+            }),
+          },
+        } as Module<CustomDescriptor>,
+      },
+      { volume: createPromisesApi(vol) }
+    );
+    const descriptor = (await service.loadProject(
+      '/workspace'
+    )) as CustomDescriptor;
 
-    expect(descriptor.module).toEqual('@baseplate/sync:workspace');
+    expect(descriptor.module).toEqual('core:test');
     expect(descriptor.name).toEqual('myProject');
+    expect(descriptor.customString).toEqual('customValue');
+    expect(descriptor.customObject).toEqual({ customNull: null });
+  });
+});
+
+describe('build', () => {
+  it('should pass descriptor to module build', () => {
+    const mockBuild = jest.fn().mockReturnValue([]);
+    const service = new SyncService({
+      'core:test': {
+        name: 'test',
+        build: mockBuild,
+      } as Module<Descriptor>,
+    });
+    const descriptor = {
+      module: 'core:test',
+      name: 'myTestBuild',
+    };
+
+    service.build(descriptor);
+
+    expect(mockBuild).toHaveBeenCalledWith(descriptor, '/');
   });
 
-  afterEach(() => {
-    mock.restore();
+  it('should return actions built up', () => {
+    const mockAction = generateMockAction();
+    const mockBuild = jest.fn().mockReturnValue([mockAction]);
+    const service = new SyncService({
+      'core:test': {
+        name: 'test',
+        build: mockBuild,
+      } as Module<Descriptor>,
+    });
+    const descriptor = {
+      module: 'core:test',
+      name: 'myTestBuild',
+    };
+
+    const actions = service.build(descriptor);
+
+    expect(actions).toHaveLength(1);
+    expect(actions[0]).toBe(mockAction);
   });
 });
--- packages/sync/src/types.ts
+++ packages/sync/src/types.ts
@@ -1,8 +1,19 @@
+import { Schema } from 'yup';
+
 export interface Descriptor {
   module: string;
   name: string;
+}
+
+export interface Module<T extends Descriptor> {
+  name: string;
+  buildDependencies?: string[];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
+  descriptorSchema?: {
+    [K in keyof Exclude<T, Descriptor>]: Schema<Exclude<T, Descriptor>[K]>;
+  };
+  parseDescriptor?: (data: T) => Promise<T>;
+  build?: (descriptor: T, directory: string) => Action[];
 }
 
 export interface Action {
