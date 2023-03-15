d3c0b24a4bfb160aa97070cbbb4e8ee68013fb07
Kingston <web@ktam.org>
2023-03-15 23:51:34 +0100

    Fix tests

--- packages/project-builder-lib/jest.config.cjs
+++ packages/project-builder-lib/jest.config.cjs
@@ -2,10 +2,23 @@ const sharedConfig = require('@baseplate/tools/jest.config.ts');
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig.json');
 
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   ...sharedConfig,
+  extensionsToTreatAsEsm: ['.ts'],
   roots: ['<rootDir>/src/'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
+    useESM: true,
   }),
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
