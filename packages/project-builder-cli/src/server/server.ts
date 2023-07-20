0fbcad435829f7d32c45e389194d9ee22187608f
Kingston <web@ktam.org>
2023-07-20 10:58:16 +0200

    Use resolved directory for ID

--- packages/project-builder-cli/src/server/server.ts
+++ packages/project-builder-cli/src/server/server.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 import fastifyHelmet from '@fastify/helmet';
 import fastifyStaticPlugin from '@fastify/static';
 import fastifyWebsocketPlugin from '@fastify/websocket';
@@ -6,6 +6,7 @@ import { fastify, FastifyInstance } from 'fastify';
 import { packageDirectory } from 'pkg-dir';
 import { logError } from '@src/services/error-logger.js';
 import { logger } from '@src/services/logger.js';
+import { expandPathWithTilde } from '@src/utils/path.js';
 import { resolveModule } from '@src/utils/resolve.js';
 import { baseplatePlugin } from './plugin.js';
 
@@ -13,6 +14,9 @@ export async function buildServer(
   directories: string[]
 ): Promise<FastifyInstance> {
   const server = fastify({ forceCloseConnections: true, logger });
+  const resolvedDirectories = directories.map((directory) =>
+    expandPathWithTilde(directory)
+  );
 
   await server.register(fastifyHelmet);
 
@@ -39,7 +43,7 @@ export async function buildServer(
 
   await server.register(fastifyWebsocketPlugin);
 
-  await server.register(baseplatePlugin, { directories });
+  await server.register(baseplatePlugin, { directories: resolvedDirectories });
 
   return server;
 }
