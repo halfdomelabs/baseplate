291b034c8fb19198566523c5fd121cc346aff0e5
Kingston <web@ktam.org>
2023-12-30 20:29:46 +0100

    Remove schema utils

--- packages/project-builder-lib/src/definition/apps/app-utils.ts
+++ packages/project-builder-lib/src/definition/apps/app-utils.ts
@@ -1,4 +1,9 @@
-import { AppConfig, ProjectConfig } from '@src/schema/index.js';
+import {
+  AppConfig,
+  BackendAppConfig,
+  ProjectConfig,
+} from '@src/schema/index.js';
+import { computeRelativePath } from '@src/utils/path.js';
 
 function byId(projectConfig: ProjectConfig, id: string): AppConfig {
   const config = projectConfig.apps.find((app) => app.id === id);
@@ -8,6 +13,38 @@ function byId(projectConfig: ProjectConfig, id: string): AppConfig {
   return config;
 }
 
+function getBackendApp(projectConfig: ProjectConfig): BackendAppConfig {
+  const backendApps = projectConfig.apps.filter(
+    (a): a is BackendAppConfig => a.type === 'backend',
+  );
+
+  if (backendApps.length > 1 || !backendApps.length) {
+    throw new Error(`Only one backend app is supported and must exist`);
+  }
+
+  const backendApp = backendApps[0];
+
+  return backendApp;
+}
+
+export function getBackendRelativePath(
+  appConfig: AppConfig,
+  backendApp: BackendAppConfig,
+): string {
+  const backendRelativePath = computeRelativePath(
+    appConfig.packageLocation
+      ? appConfig.packageLocation
+      : `packages/${appConfig.name}`,
+    backendApp.packageLocation
+      ? backendApp.packageLocation
+      : `packages/${backendApp.name}`,
+  );
+
+  return backendRelativePath;
+}
+
 export const AppUtils = {
   byId,
+  getBackendApp,
+  getBackendRelativePath,
 };
