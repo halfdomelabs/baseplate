3d22f5051ec841aac658a695b285a61a654add49
Kingston <web@ktam.org>
2024-03-23 21:07:53 +0100

    feat: Add better dark mode for storybook

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -9,8 +9,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-themes',
-    '@storybook/addon-mdx-gfm',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/react-vite',
