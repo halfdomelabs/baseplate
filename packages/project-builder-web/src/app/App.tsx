c17f80cd137f0d883ea25644de29e396175bbfd1
Matei Simtinică <matei.simtinica@gmail.com>
2024-04-05 17:44:27 +0300

    make it look closer to the initial setup

--- packages/project-builder-web/src/app/App.tsx
+++ packages/project-builder-web/src/app/App.tsx
@@ -1,40 +1,12 @@
 import { ConfirmDialog } from '@halfdomelabs/ui-components';
 import { Toaster } from 'react-hot-toast';
-import {
-  Outlet,
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import { AppTopbar } from './components/AppTopbar';
 import { ClientVersionGate } from './components/ClientVersionGate';
 import { ProjectChooserGate } from './components/ProjectChooserGate';
 import { ProjectConfigGate } from './components/ProjectConfigGate';
-import { AppLayout } from '@src/components/AppLayout/AppLayout';
 import { RefIssueDialog } from '@src/components/RefIssueDialog/RefIssueDialog';
-import NotFoundPage from '@src/pages/NotFound.page';
-import AppsPages from '@src/pages/apps';
-import { FeaturesPage } from '@src/pages/features';
-import HomePage from '@src/pages/home/home.page';
-import ModelsPages from '@src/pages/models';
-import SettingsPage from '@src/pages/settings';
 import { ErrorBoundary } from 'src/components/ErrorBoundary/ErrorBoundary';
-
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/" element={<AppLayout topbar={<AppTopbar />} />}>
-        <Route index element={<HomePage />} />
-        <Route path="/apps/*" element={<AppsPages />} />
-        <Route path="/models/*" element={<ModelsPages />} />
-        <Route path="/features/*" element={<FeaturesPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Route>,
-  ),
-);
 
 function App(): JSX.Element {
   return (
--- packages/project-builder-web/src/index.tsx
+++ packages/project-builder-web/src/index.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from './app/App';
-
 import './index.css';
+import { router } from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
