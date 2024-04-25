65f4a29a4f3edde03932ee72285b64e1120df71c
Kingston <web@ktam.org>
2024-04-25 14:22:01 +0200

    Use package-up instead of pkg-up

---
+++ packages/project-builder-server/src/plugins/plugin-discovery.ts
@@ -0,0 +1,10 @@
+import { PluginConfig } from '@halfdomelabs/project-builder-lib';
+
+/**
+ * Finds the available plugins in the project.
+ */
+export async function discoverPlugins(
+  projectDirectory: string,
+): Promise<PluginConfig[]> {
+  return [];
+}
