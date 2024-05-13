d8164b540657fd4001e783f5c0059b01de298552
Kingston <web@ktam.org>
2024-05-13 11:32:56 +0200

    Update color scheme to match design system alongside button color update

--- packages/ui-components/tailwind.config.ts
+++ packages/ui-components/tailwind.config.ts
@@ -50,14 +50,17 @@ const config: Config = {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
+          hover: 'hsl(var(--primary-hover))',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
+          hover: 'hsl(var(--secondary-hover))',
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
+          hover: 'hsl(var(--destructive-hover))',
         },
         link: {
           DEFAULT: 'hsl(var(--link))',
