7b18842b1ab4fc1d5d46b1c8d4c1d68752cee915
Kingston <web@ktam.org>
2024-05-15 15:54:28 +0200

    Adjust button styles

--- packages/ui-components/src/styles/button.ts
+++ packages/ui-components/src/styles/button.ts
@@ -8,7 +8,7 @@ export const buttonVariants = cva(
         default:
           'border border-input bg-primary text-primary-foreground hover:bg-primary-hover',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground hover:bg-destructive-hover',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
