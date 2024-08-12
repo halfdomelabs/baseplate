5f8af00aa7af030a25c165fc4933524a833f18a6
Kingston <kingston@users.noreply.github.com>
2024-08-12 10:59:51 +0200

    feat: Update topbar styles and turn project switcher into dropdown (#320)

--- packages/ui-components/src/styles/button.ts
+++ packages/ui-components/src/styles/button.ts
@@ -18,6 +18,7 @@ export const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
+        none: '',
         sm: 'h-8 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'size-8',
