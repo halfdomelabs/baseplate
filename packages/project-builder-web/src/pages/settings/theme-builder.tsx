8f5c472f4aa73537dccc2cc69f4b8bdd13132d19
Matei Simtinică <matei.simtinica@gmail.com>
2024-10-21 15:27:06 +0300

    rough layout

--- packages/project-builder-web/src/pages/settings/theme-builder.tsx
+++ packages/project-builder-web/src/pages/settings/theme-builder.tsx
@@ -9,7 +9,13 @@ import {
   useProjectDefinition,
   useResettableForm,
 } from '@halfdomelabs/project-builder-lib/web';
-import { Alert, Button, Tabs, toast } from '@halfdomelabs/ui-components';
+import {
+  Alert,
+  Button,
+  SectionList,
+  Tabs,
+  toast,
+} from '@halfdomelabs/ui-components';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useCallback, useMemo } from 'react';
 import { MdConstruction } from 'react-icons/md';
@@ -81,114 +87,117 @@ export function ThemeBuilderPage(): JSX.Element {
   }, [generateNewThemeColors]);
 
   return (
-    <div className="max-w-4xl space-y-4">
-      <h1>Theme Builder</h1>
-      <Alert>
-        <MdConstruction />
-        <Alert.Title>Work in Progress</Alert.Title>
-        <Alert.Description>
-          This page is still a work in progress. It is not being used for
-          generation at the moment.
-        </Alert.Description>
-      </Alert>
-      <p className="text-style-prose">
-        The theme of the UI is based off color variables used with{' '}
-        <a
-          href="https://ui.shadcn.com/docs/theming"
-          target="_blank"
-          rel="noreferrer"
-        >
-          the Shadn UI component library
-        </a>
-        . We generate the color variables based off the Tailwind color palette
-        structure but you can customize them as you wish.
-      </p>
-      <p>You can customize your theme by choosing one of several methods:</p>
-      <ul className="list-inside list-disc text-style-prose">
-        <li>
-          <strong>Existing Tailwind Theme:</strong> Tailwind has some{' '}
-          <a
-            href="https://tailwindcss.com/docs/customizing-colors"
-            target="_blank"
-            rel="noreferrer"
-          >
-            preset color themes
-          </a>{' '}
-          that you can use right off the bat.
-        </li>
-        <li>
-          <strong>Choose Base Color:</strong> You can choose a custom base color
-          and generate a color theme based off this color.
-        </li>
-        <li>
-          <strong>Customize Colors:</strong> You can customize the colors
-          directly for each color variable.
-        </li>
-      </ul>
+    <div className="relative h-full max-h-full pb-[var(--action-bar-height)]">
+      <div className="flex h-full max-h-full flex-1 flex-col overflow-y-auto px-6">
+        <div className="sticky top-0 space-y-2 border-b bg-background py-6">
+          <h1>Theme Builder</h1>
+          <p className="max-w-3xl text-muted-foreground">
+            The theme of the UI is based off color variables used with{' '}
+            <a
+              href="https://ui.shadcn.com/docs/theming"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              the Shadn UI component library
+            </a>
+            . We generate the color variables based off the Tailwind color
+            palette structure but you can customize them as you wish.
+          </p>
+          <Alert>
+            <MdConstruction />
+            <Alert.Title>Work in Progress</Alert.Title>
+            <Alert.Description>
+              This page is still a work in progress. It is not being used for
+              generation at the moment.
+            </Alert.Description>
+          </Alert>
+        </div>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <SectionList>
+            <SectionList.Section>
+              <SectionList.SectionHeader>
+                <SectionList.SectionTitle>
+                  Theme Palettes
+                </SectionList.SectionTitle>
+                <SectionList.SectionDescription>
+                  Pick the colors for your theme
+                </SectionList.SectionDescription>
+              </SectionList.SectionHeader>
+              <SectionList.SectionContent>
+                <Tabs defaultValue="base">
+                  <Tabs.List>
+                    <Tabs.Trigger value="base">Base</Tabs.Trigger>
+                    <Tabs.Trigger value="primary">Primary</Tabs.Trigger>
+                  </Tabs.List>
+                  <Tabs.Content value="base">
+                    <ThemePaletteEditor
+                      control={control}
+                      getValues={getValues}
+                      setValue={setValue}
+                      type="base"
+                      onShadesChange={handleShadesChange}
+                    />
+                  </Tabs.Content>
+                  <Tabs.Content value="primary">
+                    <ThemePaletteEditor
+                      control={control}
+                      getValues={getValues}
+                      setValue={setValue}
+                      type="primary"
+                      onShadesChange={handleShadesChange}
+                    />
+                  </Tabs.Content>
+                </Tabs>
+              </SectionList.SectionContent>
+            </SectionList.Section>
+            <SectionList.Section>
+              <SectionList.SectionHeader>
+                <SectionList.SectionTitle>
+                  Theme Colors
+                </SectionList.SectionTitle>
+                <SectionList.SectionDescription>
+                  Pick the colors for your theme
+                </SectionList.SectionDescription>
+              </SectionList.SectionHeader>
+              <SectionList.SectionContent>
+                <Tabs defaultValue="light">
+                  <Tabs.List>
+                    <Tabs.Trigger value="light">Light</Tabs.Trigger>
+                    <Tabs.Trigger value="dark">Dark</Tabs.Trigger>
+                  </Tabs.List>
+                  <Tabs.Content value="light">
+                    <ThemeColorEditor
+                      control={control}
+                      setValue={setValue}
+                      mode="light"
+                    />
+                  </Tabs.Content>
+                  <Tabs.Content value="dark">
+                    <ThemeColorEditor
+                      control={control}
+                      setValue={setValue}
+                      mode="dark"
+                    />
+                  </Tabs.Content>
+                </Tabs>
+                <Button
+                  onClick={() => generateNewThemeColors(true)}
+                  variant="secondary"
+                  type="button"
+                >
+                  Reset Colors
+                </Button>
+              </SectionList.SectionContent>
+            </SectionList.Section>
+          </SectionList>
 
-      <form onSubmit={onSubmit} className="space-y-4">
-        <h2>Theme Palettes</h2>
-        <Tabs defaultValue="base">
-          <Tabs.List>
-            <Tabs.Trigger value="base">Base</Tabs.Trigger>
-            <Tabs.Trigger value="primary">Primary</Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="base">
-            <ThemePaletteEditor
-              control={control}
-              getValues={getValues}
-              setValue={setValue}
-              type="base"
-              onShadesChange={handleShadesChange}
-            />
-          </Tabs.Content>
-          <Tabs.Content value="primary">
-            <ThemePaletteEditor
-              control={control}
-              getValues={getValues}
-              setValue={setValue}
-              type="primary"
-              onShadesChange={handleShadesChange}
-            />
-          </Tabs.Content>
-        </Tabs>
+          <Button type="submit">Save</Button>
 
-        <h2>Theme Colors</h2>
-        <p>Pick the colors for your theme</p>
-        <Button
-          onClick={() => generateNewThemeColors(true)}
-          variant="secondary"
-          type="button"
-        >
-          Reset Colors
-        </Button>
-
-        <Tabs defaultValue="light">
-          <Tabs.List>
-            <Tabs.Trigger value="light">Light</Tabs.Trigger>
-            <Tabs.Trigger value="dark">Dark</Tabs.Trigger>
-          </Tabs.List>
-          <Tabs.Content value="light">
-            <ThemeColorEditor
-              control={control}
-              setValue={setValue}
-              mode="light"
-            />
-          </Tabs.Content>
-          <Tabs.Content value="dark">
-            <ThemeColorEditor
-              control={control}
-              setValue={setValue}
-              mode="dark"
-            />
-          </Tabs.Content>
-        </Tabs>
-
-        <Button type="submit">Save</Button>
-
-        <h2>CSS Preview</h2>
-        <ThemeColorsCssDisplay control={control} />
-      </form>
+          <h2>CSS Preview</h2>
+          <ThemeColorsCssDisplay control={control} />
+        </form>
+      </div>
     </div>
   );
 }
