21bf99d5cff911b5454f2c918c97d673fa995fc7
Kingston <kingston@users.noreply.github.com>
2025-03-06 13:33:51 +0700

    chore: Upgrade storybook to 8.6.4 (#442)

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -1,15 +1,21 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
+import path from 'node:path';
+
+function getAbsolutePath(value: string): string {
+  return path.dirname(require.resolve(path.join(value, 'package.json')));
+}
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-dark-mode',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('storybook-dark-mode'),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   docs: {},
