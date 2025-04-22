8da04e75a3e2cff23151708936fa120db8faa65e
Kingston Tam <web@ktam.org>
2025-04-22 09:18:57 +0200

    Remove another slow test

--- packages/code-morph/src/morphers/migrate-generator-with-tasks.morpher.unit.test.ts
+++
@@ -1,4 +0,0 @@
-import migrateGeneratorWithTasksMorpher from './migrate-generator-with-tasks.morpher.js';
-import { runMorpherTests } from './tests/morpher.test-helper.js';
-
-runMorpherTests(migrateGeneratorWithTasksMorpher);
