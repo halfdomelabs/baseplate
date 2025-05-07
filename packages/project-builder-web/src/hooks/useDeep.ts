a59c7ee42efcfc0f9681d0d235e3fa1cd07fec91
Kingston <kingston@users.noreply.github.com>
2025-05-07 22:32:55 +0200

    fix: Use useDeep for useEditedModelConfig (#533)

---
+++ packages/project-builder-web/src/hooks/useDeep.ts
@@ -0,0 +1,20 @@
+import { isEqual } from 'es-toolkit';
+import { useRef } from 'react';
+
+/**
+ * A hook that returns a deeply memoized value of a selector function.
+ *
+ * @param selector - The selector function to deeply memoize.
+ */
+export function useDeep<S, U>(selector: (state: S) => U): (state: S) => U {
+  const prev = useRef<U>();
+
+  return (state) => {
+    const next = selector(state);
+    if (isEqual(prev.current, next)) {
+      return prev.current as U;
+    }
+    prev.current = next;
+    return next;
+  };
+}
