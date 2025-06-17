19fee4c4cf66500dde6e309abe506d8299992242
Kingston Tam <web@ktam.org>
2025-06-17 19:00:24 +0200

    Remove old raw template

--- packages/sync/src/templates/index.ts
+++ packages/sync/src/templates/index.ts
@@ -1,6 +1,5 @@
 export * from './constants.js';
 export * from './extractor/index.js';
 export * from './metadata/index.js';
-export * from './raw-template/index.js';
 export type * from './types.js';
 export * from './utils/index.js';
--- packages/sync/src/templates/raw-template/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './raw-template-file-extractor.js';
-export * from './render-raw-template-action.js';
-export * from './types.js';
