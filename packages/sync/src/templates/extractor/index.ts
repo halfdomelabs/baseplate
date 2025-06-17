df288a96348da1ed8da039a44f12e8d7eb1dbd31
Kingston Tam <web@ktam.org>
2025-06-17 19:01:51 +0200

    Remove old extractor logic

--- packages/sync/src/templates/extractor/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './run-template-file-extractors.js';
-export * from './template-file-extractor-test-utils.js';
-export * from './template-file-extractor.js';
--- packages/sync/src/templates/index.ts
+++ packages/sync/src/templates/index.ts
@@ -1,5 +1,4 @@
 export * from './constants.js';
-export * from './extractor/index.js';
 export * from './metadata/index.js';
 export type * from './types.js';
 export * from './utils/index.js';
