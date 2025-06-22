c11a7c9411d32467a7157c323e209822ba3dc727
Kingston Tam <web@ktam.org>
2025-06-22 12:38:52 +0200

    Try fixin gknip

--- packages/ui-components/.storybook/preview.tsx
+++ packages/ui-components/.storybook/preview.tsx
@@ -8,7 +8,7 @@ import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 
-import { Toaster } from '../src/components/Toaster/Toaster';
+import { Toaster } from '../src/components/Toaster/Toaster.js';
 import CustomTheme from './custom-theme.js';
 import { isDarkModeEnabled, setDarkModeEnabled } from './dark-mode';
 
