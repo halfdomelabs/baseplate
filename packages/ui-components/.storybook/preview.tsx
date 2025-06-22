394fc9badc160788bf714b1ffa63b1a4f5434dec
Kingston Tam <web@ktam.org>
2025-06-22 12:24:04 +0200

    Fix rename import

--- packages/ui-components/.storybook/preview.tsx
+++ packages/ui-components/.storybook/preview.tsx
@@ -9,7 +9,7 @@ import { createPortal } from 'react-dom';
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 
 import { Toaster } from '../src/components/Toaster/Toaster';
-import CustomTheme from './CustomTheme';
+import CustomTheme from './custom-theme.js';
 import { isDarkModeEnabled, setDarkModeEnabled } from './dark-mode';
 
 import '../src/styles.css';
