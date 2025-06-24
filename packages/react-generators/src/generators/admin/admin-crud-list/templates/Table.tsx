72395611afe1ccd69cbdb232b54a010e6b8333af
Kingston Tam <web@ktam.org>
2025-06-24 21:04:58 +0200

    Fix plural template

--- packages/react-generators/src/generators/admin/admin-crud-list/templates/Table.tsx
+++ packages/react-generators/src/generators/admin/admin-crud-list/templates/Table.tsx
@@ -40,7 +40,11 @@ function TPL_COMPONENT_NAME(TPL_DESTRUCTURED_PROPS: Props): ReactElement {
   }
 
   if (items.length === 0) {
-    return <Alert variant="default">No TPL_PLURAL_MODEL found.</Alert>;
+    return (
+      <Alert variant="default">
+        No <TPL_PLURAL_MODEL /> found.
+      </Alert>
+    );
   }
 
   return (
