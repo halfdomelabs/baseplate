22f70141c52c9760917445ac1a820ff23ed0a68a
Kingston Tam <web@ktam.org>
2025-07-09 15:50:31 +0200

    Add barrel exports

--- plugins/plugin-auth/src/auth/core/generators/index.ts
+++ plugins/plugin-auth/src/auth/core/generators/index.ts
@@ -1,2 +1,6 @@
+export * from './auth-email-password/index.js';
+export * from './auth-hooks/index.js';
 export * from './auth-module/index.js';
+export * from './auth-routes/index.js';
 export * from './react-auth/index.js';
+export * from './react-session/index.js';
