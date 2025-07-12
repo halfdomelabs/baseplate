d5db106bcb300058e320c73040630ff8aee08323
Kingston Tam <web@ktam.org>
2025-07-12 10:19:25 +0200

    Refactor model form to fix bug with field creation

--- packages/project-builder-web/src/routes/data/models/-components/new-model-dialog.tsx
+++ packages/project-builder-web/src/routes/data/models/-components/new-model-dialog.tsx
@@ -1,6 +1,17 @@
+import type { ModelConfigInput } from '@baseplate-dev/project-builder-lib';
 import type React from 'react';
 
-import { useBlockBeforeContinue } from '@baseplate-dev/project-builder-lib/web';
+import {
+  createModelBaseSchema,
+  FeatureUtils,
+  modelEntityType,
+  modelScalarFieldEntityType,
+} from '@baseplate-dev/project-builder-lib';
+import {
+  useBlockBeforeContinue,
+  useDefinitionSchema,
+  useProjectDefinition,
+} from '@baseplate-dev/project-builder-lib/web';
 import {
   Button,
   Dialog,
@@ -13,8 +24,12 @@ import {
   DialogTrigger,
   useControlledState,
 } from '@baseplate-dev/ui-components';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from '@tanstack/react-router';
+import { sortBy } from 'es-toolkit';
+import { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
 
-import { useModelForm } from '../-hooks/use-model-form.js';
 import { ModelInfoForm } from '../edit.$key/-components/model-info-form.js';
 
 interface NewModelDialogProps {
@@ -23,22 +38,108 @@ interface NewModelDialogProps {
   onOpenChange?: (open: boolean) => void;
 }
 
+function createNewModel(): ModelConfigInput {
+  const idFieldId = modelScalarFieldEntityType.generateNewId();
+  return {
+    id: modelEntityType.generateNewId(),
+    name: '',
+    featureRef: '',
+    service: {
+      create: { enabled: false },
+      update: { enabled: false },
+      delete: { enabled: false },
+      transformers: [],
+    },
+    model: {
+      primaryKeyFieldRefs: [idFieldId],
+      fields: [
+        {
+          id: idFieldId,
+          name: 'id',
+          type: 'uuid',
+          isOptional: false,
+          options: {
+            default: '',
+            genUuid: true,
+          },
+        },
+      ],
+    },
+  };
+}
+
 export function NewModelDialog({
   children,
   open,
   onOpenChange,
 }: NewModelDialogProps): React.JSX.Element {
   const [isOpen, setIsOpen] = useControlledState(open, onOpenChange, false);
-  const {
-    onSubmit,
-    form: { control },
-  } = useModelForm({
-    isCreate: true,
-    modelKey: undefined,
-    onSubmitSuccess: () => {
-      setIsOpen(false);
-    },
+  const { definition, saveDefinitionWithFeedback } = useProjectDefinition();
+  // memoize it to keep the same key when resetting
+  const baseModelSchema = useDefinitionSchema(createModelBaseSchema);
+
+  const defaultValues = useMemo(() => createNewModel(), []);
+  const navigate = useNavigate();
+
+  const { handleSubmit, reset, setError, control } = useForm({
+    resolver: zodResolver(baseModelSchema),
+    defaultValues,
   });
+
+  const onSubmit = useMemo(
+    () =>
+      handleSubmit((data) => {
+        // check for models with the same name
+        const existingModel = definition.models.find(
+          (m) => m.name.toLowerCase() === data.name.toLowerCase(),
+        );
+        if (existingModel) {
+          setError('name', {
+            message: `Model with name ${data.name} already exists.`,
+          });
+          return;
+        }
+
+        return saveDefinitionWithFeedback(
+          (draftConfig) => {
+            // create feature if a new feature exists
+            const updatedModel = { ...data };
+            updatedModel.featureRef =
+              FeatureUtils.ensureFeatureByNameRecursively(
+                draftConfig,
+                updatedModel.featureRef,
+              );
+            draftConfig.models = sortBy(
+              [
+                ...draftConfig.models.filter((m) => m.id !== updatedModel.id),
+                updatedModel,
+              ],
+              [(m) => m.name],
+            );
+          },
+          {
+            successMessage: 'Successfully created model!',
+            onSuccess: () => {
+              navigate({
+                to: '/data/models/edit/$key',
+                params: { key: modelEntityType.keyFromId(data.id) },
+              });
+              reset(createNewModel());
+              setIsOpen(false);
+            },
+          },
+        );
+      }),
+    [
+      reset,
+      setError,
+      handleSubmit,
+      saveDefinitionWithFeedback,
+      navigate,
+      definition,
+      setIsOpen,
+    ],
+  );
 
   const blockBeforeContinue = useBlockBeforeContinue();
 
--- packages/utils/src/json/index.ts
+++ packages/utils/src/json/index.ts
@@ -1,2 +1,3 @@
+export * from './json-deep-clone.js';
 export * from './stringify-pretty-compact.js';
 export * from './stringify-pretty-stable.js';
