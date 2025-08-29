0ecc422e0bb6acfb1276240868ff00654c06b776
Kingston <web@ktam.org>
2025-08-29 08:18:06 -0700

    Properly support Claude

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,7 @@ set -e
 # Fix permissions on pnpm store and remove sudo permissions
 sudo chown vscode:vscode node_modules
 sudo chown vscode:vscode /home/vscode/.local/share/pnpm
+sudo chown vscode:vscode /home/vscode/.claude
 sudo rm /etc/sudoers.d/vscode
 
 # Trust mise and install dependencies
