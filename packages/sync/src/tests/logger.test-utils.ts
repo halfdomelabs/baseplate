6450364e297da8afc4b4d83a8a6e890ebf5bb723
Kingston <web@ktam.org>
2025-08-31 14:52:17 +0200

    Fix formatting of logger test utils

--- packages/sync/src/tests/logger.test-utils.ts
+++ packages/sync/src/tests/logger.test-utils.ts
@@ -11,22 +11,26 @@ export function createTestLogger(): TestLogger {
   let [errorOutput, warnOutput, infoOutput, debugOutput] = Array.from({
     length: 4,
   }).fill('') as string[];
-  
+
   const formatMessage = (messageOrObj: unknown, message?: string): string => {
     if (typeof messageOrObj === 'string') {
       return messageOrObj;
     } else if (typeof messageOrObj === 'object' && messageOrObj !== null) {
       const obj = messageOrObj as Record<string, unknown>;
-      const msg = message ?? 
-                   (typeof obj.message === 'string' ? obj.message : 
-                    typeof obj.msg === 'string' ? obj.msg : '');
+      const msg =
+        message ??
+        (typeof obj.message === 'string'
+          ? obj.message
+          : typeof obj.msg === 'string'
+            ? obj.msg
+            : '');
       const metadata = JSON.stringify(messageOrObj);
       return msg ? `${msg} ${metadata}` : metadata;
     } else {
       return String(messageOrObj);
     }
   };
-  
+
   return {
     error: (messageOrObj: unknown, message?: string) => {
       errorOutput += `${formatMessage(messageOrObj, message)}\n`;
