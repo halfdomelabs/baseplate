29c76691248b2ff5aac4d2e7f8c007a80531915d
Kingston <web@ktam.org>
2025-08-31 13:59:19 +0200

    Add delete/extract/generate/list operations to TRPC

--- packages/project-builder-server/src/dev-server/api/router.ts
+++ packages/project-builder-server/src/dev-server/api/router.ts
@@ -1,10 +1,22 @@
 import { diffProjectAction } from '#src/actions/diff/diff-project.action.js';
+import {
+  deleteTemplateAction,
+  extractTemplatesAction,
+  generateTemplatesAction,
+  listTemplatesAction,
+} from '#src/actions/index.js';
 
 import { devRouter, devTrpcActionBuilder } from './trpc.js';
 
 export const devServerRouter = devRouter({
   diff: devRouter({
     diffProject: devTrpcActionBuilder.mutation(diffProjectAction),
+  }),
+  templateExtractor: devRouter({
+    delete: devTrpcActionBuilder.mutation(deleteTemplateAction),
+    extract: devTrpcActionBuilder.mutation(extractTemplatesAction),
+    generate: devTrpcActionBuilder.mutation(generateTemplatesAction),
+    list: devTrpcActionBuilder.query(listTemplatesAction),
   }),
 });
 
