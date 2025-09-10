ad81e21f1686e082e08abf1f20ac6917b8d5d2b1
Kingston <web@ktam.org>
2025-09-10 18:57:46 +0200

    fix: Admin section paths with spaces in them

--- packages/project-builder-server/src/utils/case.ts
+++ packages/project-builder-server/src/utils/case.ts
@@ -1,15 +1,4 @@
-import { dasherize, titleize, underscore } from 'inflection';
-
-function lowerCaseFirst(str: string): string {
-  if (str.length === 0) {
-    return str;
-  }
-  return str.charAt(0).toLowerCase() + str.slice(1);
-}
-
-export function dasherizeCamel(str: string): string {
-  return dasherize(underscore(lowerCaseFirst(str)));
-}
+import { titleize, underscore } from 'inflection';
 
 export function titleizeCamel(str: string): string {
   return titleize(underscore(str));
