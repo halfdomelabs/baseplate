a2fbc80847197d07205486a9c690aa450ae2c54c
Kingston <web@ktam.org>
2025-09-12 15:34:42 +0200

    Run pnpm test:generate

--- tests/simple/packages/web/src/components/ui/select.tsx
+++ tests/simple/packages/web/src/components/ui/select.tsx
@@ -101,7 +101,7 @@ function SelectContent({
               className={cn(
                 'h-full w-full rounded-[inherit] p-1',
                 position === 'popper'
-                  ? 'max-h-[min(var(--max-popper-height),var(--radix-select-content-available-height))] w-full min-w-(--radix-select-trigger-width)'
+                  ? 'min-w-(--radix-select-trigger-width) max-h-[min(var(--max-popper-height),var(--radix-select-content-available-height))] w-full'
                   : 'max-h-(--max-popper-height)',
               )}
               style={
@@ -130,7 +130,7 @@ function SelectLabel({
   return (
     <SelectPrimitive.Label
       data-slot="select-label"
-      className={cn('px-2 py-1.5 text-xs text-muted-foreground', className)}
+      className={cn('text-muted-foreground px-2 py-1.5 text-xs', className)}
       {...props}
     />
   );
@@ -164,7 +164,7 @@ function SelectSeparator({
   return (
     <SelectPrimitive.Separator
       data-slot="select-separator"
-      className={cn('pointer-events-none -mx-1 my-1 h-px bg-border', className)}
+      className={cn('bg-border pointer-events-none -mx-1 my-1 h-px', className)}
       {...props}
     />
   );
