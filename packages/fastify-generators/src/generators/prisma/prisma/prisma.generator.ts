81ecc12c0e11ef501f7a63e196f868abe2bfab0f
Kingston <web@ktam.org>
2025-10-14 23:37:32 +0200

    Add back postinstall until we can commit prisma to the codebase

--- packages/fastify-generators/src/generators/prisma/prisma/prisma.generator.ts
+++ packages/fastify-generators/src/generators/prisma/prisma/prisma.generator.ts
@@ -109,6 +109,10 @@ export const prismaGenerator = createGenerator({
           ]),
           dev: extractPackageVersions(FASTIFY_PACKAGES, ['prisma']),
         });
+        // add prisma generate script to postinstall for pnpm (https://github.com/prisma/prisma/issues/6603)
+        node.scripts.mergeObj({
+          postinstall: 'prisma generate',
+        });
       },
     }),
     eslint: createGeneratorTask({
