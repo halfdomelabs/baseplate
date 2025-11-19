e224dec116e9b7f77731537fdec44e658d0aaefa
Kingston <web@ktam.org>
2025-11-19 14:14:47 +0100

    Fix up knip

--- packages/fastify-generators/src/generators/prisma/_shared/crud-method/primary-key-input.ts
+++ packages/fastify-generators/src/generators/prisma/_shared/crud-method/primary-key-input.ts
@@ -1,19 +1,5 @@
-import type { TsHoistedFragment } from '@baseplate-dev/core-generators';
-
-import { tsHoistedFragment } from '@baseplate-dev/core-generators';
-import { quot } from '@baseplate-dev/utils';
-
 import type { PrismaOutputModel } from '#src/types/prisma-output.js';
 import type { ServiceOutputDtoField } from '#src/types/service-output.js';
-
-import { getScalarFieldTypeInfo } from '#src/types/field-types.js';
-
-interface PrimaryKeyOutput {
-  argumentName: string;
-  whereClause: string;
-  headerTypeBlock?: TsHoistedFragment;
-  argumentType: string;
-}
 
 export function getPrimaryKeyDefinition(
   model: PrismaOutputModel,
@@ -78,49 +64,4 @@ export function getModelIdFieldName(model: PrismaOutputModel): string {
 
   // handle multiple primary key case
   return idFields.join('_');
-}
-
-function getPrimaryKeyExpressions(
-  model: PrismaOutputModel,
-): PrimaryKeyOutput {
-  const { idFields, fields } = model;
-  if (!idFields?.length) {
-    throw new Error(`Model ${model.name} has no primary key`);
-  }
-
-  const idFieldName = getModelIdFieldName(model);
-
-  if (idFields.length === 1) {
-    // handle trivial one primary key case
-    const idField = fields.find((f) => f.name === idFieldName);
-
-    if (!idField || idField.type !== 'scalar') {
-      throw new Error(`Model ${model.name} must have a scalar primary key`);
-    }
-
-    const argumentType = getScalarFieldTypeInfo(
-      idField.scalarType,
-    ).typescriptType;
-
-    return {
-      argumentName: idFieldName,
-      whereClause: `{ ${idFieldName} }`,
-      argumentType,
-    };
-  }
-
-  // handle multiple primary key case
-  const primaryKeyInputName = `${model.name}PrimaryKey`;
-
-  const headerTypeBlock = tsHoistedFragment(
-    `input-type:${primaryKeyInputName}`,
-    `export type ${primaryKeyInputName} = Pick<${model.name}, ${idFields.map(quot).join(' | ')}>`,
-  );
-
-  return {
-    argumentName: idFieldName,
-    whereClause: `{ ${idFieldName} }`,
-    headerTypeBlock,
-    argumentType: primaryKeyInputName,
-  };
 }
