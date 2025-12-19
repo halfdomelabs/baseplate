21395d4c86fbb239682ed6138d011615d228e768
Kingston <web@ktam.org>
2025-12-19 14:51:07 +0100

    Upgrade Storybook and TRPC

--- packages/ui-components/.storybook/main.ts
+++ packages/ui-components/.storybook/main.ts
@@ -1,20 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
-import path from 'node:path';
-
-function getAbsolutePath(value: string): string {
-  return path.dirname(require.resolve(path.join(value, 'package.json')));
-}
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-themes'),
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-themes',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {},
