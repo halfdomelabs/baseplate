c28def81e771f7f6bd8f012396ee2701d84d649a
Kingston <web@ktam.org>
2025-12-31 12:28:25 +0100

    feat: Add automatic cleanup of orphaned templates during extraction

--- packages/sync/src/templates/metadata/index.ts
+++ packages/sync/src/templates/metadata/index.ts
@@ -1,4 +1,5 @@
 export * from './delete-metadata.js';
 export * from './metadata.js';
 export * from './read-template-info-files.js';
+export * from './remove-template-info-entry.js';
 export * from './write-template-info-files.js';
