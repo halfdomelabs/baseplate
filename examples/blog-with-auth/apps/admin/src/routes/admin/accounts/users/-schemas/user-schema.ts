fd818ea6afce1f63bbc5192198e6140197e99e30
Kingston <web@ktam.org>
2026-01-04 23:58:41 +0100

    Small tweaks to make the diff smaller

--- examples/blog-with-auth/apps/admin/src/routes/admin/accounts/users/-schemas/user-schema.ts
+++ examples/blog-with-auth/apps/admin/src/routes/admin/accounts/users/-schemas/user-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
-/* TPL_SCHEMA_NAME=userEditFormSchema */
 /* TPL_FORM_DATA_NAME=UserFormData */
+/* TPL_SCHEMA_NAME=userEditFormSchema */
 
 export const userEditFormSchema = z.object(
   /* TPL_SCHEMA_OBJECT:START */ {
