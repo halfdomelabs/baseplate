57deaae7becf62ac67f3a7d6baa8b7eb050be6d5
Kingston <kingston@users.noreply.github.com>
2026-02-15 20:01:26 +0100

    chore: Upgrade AWS SDK client S3 to 3.990.0 (#764)

--- plugins/plugin-storage/src/constants/storage-packages.ts
+++ plugins/plugin-storage/src/constants/storage-packages.ts
@@ -1,8 +1,8 @@
 export const STORAGE_PACKAGES = {
-  '@aws-sdk/client-s3': '3.934.0',
-  '@aws-sdk/lib-storage': '3.934.0',
-  '@aws-sdk/s3-presigned-post': '3.934.0',
-  '@aws-sdk/s3-request-presigner': '3.934.0',
+  '@aws-sdk/client-s3': '3.990.0',
+  '@aws-sdk/lib-storage': '3.990.0',
+  '@aws-sdk/s3-presigned-post': '3.990.0',
+  '@aws-sdk/s3-request-presigner': '3.990.0',
   'mime-types': '2.1.35',
   '@types/mime-types': '2.1.1',
 
