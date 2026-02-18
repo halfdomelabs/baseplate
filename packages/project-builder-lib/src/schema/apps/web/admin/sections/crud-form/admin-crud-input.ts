3257c2cc91225cd2d7e91c106d06de821601bec6
Kingston <web@ktam.org>
2026-02-18 23:04:39 +0100

    Fix up admin to use discriminatedUnion

--- packages/project-builder-lib/src/schema/apps/web/admin/sections/crud-form/admin-crud-input.ts
+++ packages/project-builder-lib/src/schema/apps/web/admin/sections/crud-form/admin-crud-input.ts
@@ -1,19 +1,32 @@
+import z from 'zod';
+
+import type { def } from '#src/schema/creator/index.js';
+
 import { definitionSchemaWithSlots } from '#src/schema/creator/schema-creator.js';
 import { modelEntityType } from '#src/schema/models/index.js';
 
+import type { baseAdminCrudInputSchema } from './types.js';
+
 import { adminSectionEntityType } from '../types.js';
 import { adminCrudInputSpec } from './admin-input-spec.js';
-import { baseAdminCrudInputSchema } from './types.js';
 
 export const createAdminCrudInputSchema = definitionSchemaWithSlots(
   { modelSlot: modelEntityType, adminSectionSlot: adminSectionEntityType },
   (ctx, slots) => {
-    const adminCrudInputs = ctx.plugins.use(adminCrudInputSpec).inputs;
-
-    return baseAdminCrudInputSchema.transform((data) => {
-      const inputDef = adminCrudInputs.get(data.type);
-      if (!inputDef) return data;
-      return inputDef.createSchema(ctx, slots).parse(data);
-    });
+    const { inputs } = ctx.plugins.use(adminCrudInputSpec);
+    return z.discriminatedUnion(
+      'type',
+      [...inputs.values()].map((input) => input.createSchema(ctx, slots)) as [
+        typeof baseAdminCrudInputSchema,
+      ],
+    );
   },
 );
+
+export type AdminCrudInputConfig = def.InferOutput<
+  typeof createAdminCrudInputSchema
+>;
+
+export type AdminCrudInputConfigInput = def.InferInput<
+  typeof createAdminCrudInputSchema
+>;
