2547117deca14f1af004bb42eb462b7b4f1b9a57
Kingston <web@ktam.org>
2026-02-18 21:40:03 +0100

    Clean up tests

--- packages/project-builder-lib/src/schema/definition.test-helper.ts
+++ packages/project-builder-lib/src/schema/definition.test-helper.ts
@@ -2,10 +2,22 @@ import { faker } from '@faker-js/faker';
 import { capitalize } from 'es-toolkit';
 
 import type { FeatureConfig } from './features/feature.js';
-import type { ModelConfig, ModelScalarFieldConfig } from './models/index.js';
+import type {
+  ModelConfig,
+  ModelRelationFieldConfig,
+  ModelScalarFieldConfig,
+  ModelScalarFieldConfigInput,
+  ModelUniqueConstraintConfig,
+} from './models/index.js';
 
 import { featureEntityType } from './features/feature.js';
-import { modelEntityType, modelScalarFieldEntityType } from './models/index.js';
+import {
+  modelEntityType,
+  modelForeignRelationEntityType,
+  modelLocalRelationEntityType,
+  modelScalarFieldEntityType,
+  modelUniqueConstraintEntityType,
+} from './models/index.js';
 
 export function createTestFeature(
   feature: Partial<FeatureConfig> = {},
@@ -17,14 +29,11 @@ export function createTestFeature(
   };
 }
 
-export function createTestModel(
-  featureId: string,
-  model: Partial<ModelConfig> = {},
-): ModelConfig {
+export function createTestModel(model: Partial<ModelConfig> = {}): ModelConfig {
   return {
     id: modelEntityType.generateNewId(),
     name: capitalize(faker.word.noun()),
-    featureRef: featureId,
+    featureRef: 'mockFeature',
     model: {
       fields: [
         {
@@ -49,18 +58,46 @@ export function createTestModel(
   };
 }
 
-export function createTestScalarField(
-  field: Partial<ModelScalarFieldConfig> = {},
-): ModelScalarFieldConfig {
+export function createTestScalarField<
+  TType extends ModelScalarFieldConfig['type'] = 'string',
+>(
+  field: Partial<ModelScalarFieldConfigInput & { type: TType }> = {},
+): ModelScalarFieldConfig & { type: TType } {
   return {
     id: modelScalarFieldEntityType.generateNewId(),
     name: faker.word.noun(),
     type: 'string',
     isOptional: false,
+    ...field,
     options: {
       default: '123',
       ...field.options,
     },
-    ...field,
+  } as ModelScalarFieldConfig & { type: TType };
+}
+
+export function createTestRelationField(
+  relation?: Partial<ModelRelationFieldConfig>,
+): ModelRelationFieldConfig {
+  return {
+    id: modelLocalRelationEntityType.generateNewId(),
+    foreignId: modelForeignRelationEntityType.generateNewId(),
+    name: 'mockRelation',
+    modelRef: 'mockModel',
+    foreignRelationName: 'mockForeignRelation',
+    references: [],
+    onDelete: 'Cascade',
+    onUpdate: 'Restrict',
+    ...relation,
+  };
+}
+
+export function createTestUniqueConstraint(
+  constraint?: Partial<ModelUniqueConstraintConfig>,
+): ModelUniqueConstraintConfig {
+  return {
+    id: modelUniqueConstraintEntityType.generateNewId(),
+    fields: [],
+    ...constraint,
   };
 }
