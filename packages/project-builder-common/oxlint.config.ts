b7c9ce3e349fce5ee0fce5a3459c3f6d1894f0c0
Kingston <web@ktam.org>
2026-03-15 16:33:19 +0100

    Add oxlint configs to all packages

--- packages/project-builder-common/oxlint.config.ts
+++
@@ -1,9 +0,0 @@
-import oxlintConfigBase, {
-  oxlintIgnorePatterns,
-} from '@baseplate-dev/tools/oxlint-config-base';
-import { defineConfig } from 'oxlint';
-
-export default defineConfig({
-  extends: [oxlintConfigBase],
-  ignorePatterns: oxlintIgnorePatterns,
-});
