78da4ba59862389310cc6bc039f91d233f8b1727
Kingston <web@ktam.org>
2026-03-18 15:44:16 -0700

    Convert over todo-with-better-auth

--- examples/todo-with-better-auth/apps/backend/baseplate/generated/src/modules/accounts/auth/index.ts
+++ examples/todo-with-better-auth/apps/backend/baseplate/generated/src/modules/accounts/auth/index.ts
@@ -4,12 +4,8 @@ import { authPlugin } from './plugins/auth.plugin.js';
 import { betterAuthPlugin } from './plugins/better-auth.plugin.js';
 
 /* TPL_IMPORTS:START */
-import './schema/user-role.object-type.js';
 import './schema/admin-auth.mutations.js';
 import './schema/user-session.queries.js';
-import './schema/user.mutations.js';
-import './schema/user.object-type.js';
-import './schema/user.queries.js';
 /* TPL_IMPORTS:END */
 
 export const /* TPL_MODULE_NAME:START */ authModule /* TPL_MODULE_NAME:END */ =
