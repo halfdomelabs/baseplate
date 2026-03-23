5d4e378754b108e486b8f6a718b208b93c7c964d
Kingston <web@ktam.org>
2026-03-23 03:07:25 -0700

    Clean up excess role

--- examples/blog-with-auth/apps/backend/baseplate/generated/src/modules/accounts/users/authorizers/user.query-filter.ts
+++ examples/blog-with-auth/apps/backend/baseplate/generated/src/modules/accounts/users/authorizers/user.query-filter.ts
@@ -3,7 +3,6 @@ import { createModelQueryFilter } from '@src/utils/query-filters.js';
 export const userQueryFilter = createModelQueryFilter({
   model: 'user',
   roles: {
-    owner: (ctx) => (ctx.auth.userId != null ? { id: ctx.auth.userId } : false),
     self: (ctx) => (ctx.auth.userId != null ? { id: ctx.auth.userId } : false),
   },
 });
