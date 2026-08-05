b560c3943c9a43a3a956c3f30d7bb79d36e2e10f
Kingston <web@ktam.org>
2026-08-05 11:12:10 +0200

    Fix template for app-module

--- packages/fastify-generators/src/generators/core/app-module/templates/index.ts
+++ packages/fastify-generators/src/generators/core/app-module/templates/index.ts
@@ -2,7 +2,6 @@
 
 import { defineAppModule } from '%appModuleSetupImports';
 
-import './schema/blog-post-like.mutations.js';
 TPL_IMPORTS;
 
 export const TPL_MODULE_NAME = defineAppModule(TPL_MODULE_CONTENTS);
